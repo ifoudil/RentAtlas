40cb37474e164a95dce9d1f9a71b609976b9ea9b
Ludivine Levallois <levallois.ludivine@gmail.com>
2025-11-30 19:41:15 +0100

    fix: can get map coordinates for GEOJson

--- fetchMap.js
+++ fetchMap.js
@@ -1,51 +1,45 @@
-// const rawUrl = "https://commons.wikimedia.org/w/index.php?title=Data:Ain.map&action=raw";
-
-// fetch(rawUrlToApi(rawUrl))
-  // .then(r => r.json())
-  // .then(json => {
-    // const page = Object.values(json.query.pages)[0];
-    // const content = JSON.parse(page.revisions[0]["*"]);
-    // console.log(content.data);
-  // })
-  // .catch(console.error);
-
-// function rawUrlToApi(url) {
-  // const title = url.match(/title=(Data:[^&]+)/)?.[1];
-  // return title
-    // ? `https://commons.wikimedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=${encodeURIComponent(title)}&origin=*`
-    // : null;
-// }
-
 async function getData(code) {
-  const endpoint = 'https://corsproxy.io/?url=' + encodeURIComponent('http://localhost:7200/repositories/loyer');
+  let endpoint = 'http://localhost:7200/repositories/loyer';
 
-  const query = 
+  let query = 
   `PREFIX rcw: <https://cours.iut-orsay.fr/rcw/>
     SELECT DISTINCT ?map
     WHERE {
-      ?dept rcw:code "${code}" ;
+      ?dept rcw:codeDep "${code}" ;
             rcw:map ?map .
     }`;
 
+    let url = endpoint + '?query=' + encodeURIComponent(query) ;
+
+    let data = await fetch(url).then(response => response.text());
+    let result = await getGEOJson(data)
+    return result
+}
+
+async function getGEOJson(data){
   try {
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/sparql-query',
-        'Accept': 'application/sparql-results+json'
-      },
-      body: query
-    });
+    let response = await fetch(getMapURL(data));
+    let json = await response.json();
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+    const page = Object.values(json.query.pages)[0];
+    const content = JSON.parse(page.revisions[0]["*"]);
 
-    const data = await response.json();
-    return data.results.bindings.map(b => b.map.value);
-
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
+    return content.data; 
+  } 
+  catch (err) {
+    console.error(err);
+    return null;
   }
+}
+
+
+function getMapURL(url) {
+  if (!url) return null;
+
+  const index = url.indexOf("Data:");
+  if (index === -1) return null;
+
+  const title = decodeURIComponent(url.substring(index).trim());
+
+  return `https://commons.wikimedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=${encodeURIComponent(title)}&origin=*`;
 }
